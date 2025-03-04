f44af20ce62a3df7871f1f21ef4c5d807092aa35
YERIEL-RYU <yeriel.ryu@gmail.com>
2025-03-04 21:54:04 +0900

    pulication page ui

--- frontend/src/components/page/Publication.tsx
+++ frontend/src/components/page/Publication.tsx
@@ -1,5 +1,29 @@
-const Publication = () => {
-  return <div>publication</div>
-};
+import SubSideMenu from '../SubSideMenu'
+import PageTitle from '../PageTitle'
 
-export default Publication;
+const Publication = () => {
+  const menuList = [
+    { name: 'Publications', url: '#publications' },
+    { name: 'lectures', url: '#lectures' },
+  ]
+
+  return (
+    <main className="space-y-9 mb-20 relative">
+      <section className="xl:w-[1170px] lg:w-[970px] md:w-[750px] sm:w-[450px] mx-auto">
+        <div className="flex mt-4">
+          <SubSideMenu title="Team" menuList={menuList} />
+          <div className="w-full lg:ml-72 md:ml-0 sm:ml-0">
+            <div id="publications" className="scroll-m-[260px]">
+              <PageTitle title="publications" page="Publication" />
+            </div>
+            <div id="lectures" className="scroll-m-[260px]">
+              <PageTitle title="lectures" page="Publication" />
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  )
+}
+
+export default Publication
---
+++ frontend/src/data/Publication.ts
@@ -0,0 +1,74 @@
+export const publication = [
+  {
+    code: 12, 
+    title: '“Low gamma-butyrobetaine dioxygenase (BBOX1) expression as a prognostic biomarker in patients with clear cell renal cell carcinoma: a machine learning approach.”',
+    author: 'Kim KS, Moon KM, Min KW, Jung WY, Shin SJ, Lee SW, Kwon MJ, Kim DH, Oh S, Noh YK.',
+    publication: 'J Pathol Clin Res. 2023 May;9(3):236-248. doi: 10.1002/cjp2.315. PMID: 36864013(Co-First)'
+  },
+  {
+    code: 11, 
+    title: '“Deep learning model to predict Epstein-Barr virus associated gastric cancer in histology.”',
+    author: 'Jeong Y, Cho CE, Kim JE, Lee J, Kim N, Jung WY, Sung J, Kim JH, Lee YJ, Jung J, Pyo J, Song J, Park J, Moon KM, Ahn S.',
+    publication: 'Sci Rep. 2022 Nov 2;12(1):18466. doi: 10.1038/s41598-022-22731-x. PMID: 36323712 (Co-Corresponding)'
+  },
+  {
+    code: 10, 
+    title: '“How Many Private Data Are Needed for Deep Learning in Lung Nodule Detection on CT Scans? A Retrospective Multicenter Study.',
+    author: 'Son JW, Hong JY, Kim Y, Kim WJ, Shin DY, Choi HS, Bak SH, Moon KM.',
+    publication: 'Cancers (Basel). 2022 Jun 28;14(13):3174. doi: 10.3390/cancers14133174. PMID: 35804946 (Co-Corresponding)'
+  },
+  {
+    code: 9, 
+    title: '“Machine Learning Models for Predicting Mortality in 7472 Very Low Birth Weight Infants Using Data from a Nationwide Neonatal Network.”',
+    author: 'Do HJ, Moon KM, Jin HS.',
+    publication: 'Diagnostics (Basel). 2022 Mar 3;12(3):625. doi: 10.3390/diagnostics12030625. PMID: 35328178 (Co-Corresponding)'
+  },
+  {
+    code: 8, 
+    title: '“Exercise-Induced Myokines can Explain the Importance of Physical Activity in the Elderly: An Overview.”',
+    author: 'Kwon JH, Moon KM, Min KW.',
+    publication: 'Healthcare (Basel). 2020 Oct 1;8(4):378. doi: 10.3390/healthcare8040378. PMID: 33019579 (Co-Corresponding)'
+  },
+  {
+    code: 7, 
+    title: '“Clinical Characteristics of and Risk Factors for Fever after Endobronchial Ultrasound-Guided Transbronchial Needle Aspiration: A Retrospective Study Involving 6336 Patients.”',
+    author: 'Moon KM, Choi CM, Ji W, Lee JS, Lee SW, Jo KW, Song JW, Lee JC.',
+    publication: 'J Clin Med. 2020 Jan 6;9(1):152. doi: 10.3390/jcm9010152. PMID: 31935941 (First)'
+  },
+  {
+    code: 6, 
+    title: '“Resident Working Hour Restrictions Increased the Workload of the Medical Emergency Team: A Retrospective Observational Study.”',
+    author: 'Moon KM, Huh JW, Park S, Hong SB, Lim CM, Koh Y.',
+    publication: 'J Patient Saf. 2019 Dec;15(4):e94-e97. doi: 10.1097/PTS.0000000000000629. PMID: 31764533 (First)'
+  },
+  {
+    code: 5, 
+    title: '“Higher Acid-Base Imbalance Associated with Respiratory Failure Could Decrease the Survival of Patients with Scrub Typhus during Intensive Care Unit Stay: A Gene Set Enrichment Analysis.”',
+    author: 'Moon KM, Min KW, Kim MH, Kim DH, Son BK, Oh Y, Jung W, Kwon M, Kwon OY.',
+    publication: 'J Clin Med. 2019 Oct 2;8(10):1580. doi: 10.3390/jcm8101580. PMID: 31581612 (First)'
+  },
+  {
+    code: 4, 
+    title: '“Risk Factors for Mechanical Ventilation in Patients with Scrub Typhus Admitted to Intensive Care Unit at a University Hospital.”',
+    author: 'Moon KM, Han MS, Rim CB, Lee JH, Kang MS, Kim JH, Kim SI, Jung SY, Cho Y.',
+    publication: 'Tuberc Respir Dis (Seoul). 2016 Jan;79(1):31-6. doi: 10.4046/trd.2016.79.1.31. PMID: 26770232 (First)'
+  },
+  {
+    code: 3, 
+    title: '“Transfusion Related Acute Lung Injury after Cesarean Section in a Patient with HELLP Syndrome.”',
+    author: 'Moon KM, Han MS, Rim CB, Kim SR, Shin SH, Kang MS, Lee JH, Kim J, Kim SI.',
+    publication: 'Korean J Fam Med. 2016 Jan;37(1):71-4. doi: 10.4082/kjfm.2016.37.1.71. PMID: 26885326 (First)'
+  },
+  {
+    code: 2, 
+    title: '“Agranulocytosis induced by ethambutol in a patient with pulmonary tuberculosis.”',
+    author: 'Moon KM, Han MS, Chung SH, Kim JR, Kim JY, Jung SY, Cho Y.',
+    publication: 'Tuberc Respir Dis (Seoul). 2015 Apr;78(2):125-7. doi: 10.4046/trd.2015.78.2.125. PMID: 25861347 (First)'
+  },
+  {
+    code: 1, 
+    title: '“Ultrasonographic scoring system score versus liver stiffness measurement in prediction of cirrhosis.”',
+    author: 'Moon KM, Kim G, Baik SK, Choi E, Kim MY, Kim HA, Cho MY, Shin SY, Kim JM, Park HJ, Kwon SO, Eom YW',
+    publication: 'Clin Mol Hepatol. 2013 Dec;19(4):389-98. doi: 10.3350/cmh.2013.19.4.389. PMID: 24459644 (First)'
+  },
+]
