156b4fe8179320af8c6bd2b342820b021ad66054
YERIEL-RYU <yeriel.ryu@gmail.com>
2025-03-26 10:57:54 +0900

    dist folder push

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Vercel에서는 '/'로 설정
+  base: '/gastrerology_labs_blog/', // Vercel에서는 '/'로 설정
 });
