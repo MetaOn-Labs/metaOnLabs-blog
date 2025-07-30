e647df482d78950800af8645e2ff9a4e35073e50
YERIEL-RYU <yeriel.ryu@gmail.com>
2025-07-30 19:10:05 +0900

    Topic : 슬랙 token Env
    Message :
    slack token env로 이동

--- frontend/src/components/Contact/index.tsx
+++ frontend/src/components/Contact/index.tsx
@@ -1,8 +1,65 @@
+<<<<<<< HEAD
+=======
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useState } from 'react'
+>>>>>>> 3695e21 (Topic : 슬랙 token Env)
 import DashboardTitle from '../DashboardTitle'
 import { SectionLayout } from '../layout/Layout'
 import { Card } from '@material-tailwind/react'
 
 const Contact = () => {
+<<<<<<< HEAD
+=======
+  const [formValue, setFormValue] = useState({
+    name: '',
+    email: '',
+    organization: '',
+    message: '',
+  })
+
+  const onClickSend = async () => {
+    const url = import.meta.env.VITE_SLACK_WEBHOOK_URL
+    const body = {
+      text: `📤 *Blog contact 알림*\n• name: ${formValue.name}\n• email: ${formValue.email}\n• modality: ${formValue.organization}\n• message: ${formValue.message}`,
+    }
+    await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(body),
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    }).then(({ status }: any) => {
+      if (status === 200) {
+        window.alert('성공적으로 보내졌습니다.')
+        setFormValue({
+          name: '',
+          email: '',
+          organization: '',
+          message: '',
+        })
+      } else {
+        window.alert('메세지 전송에 실패하였습니다.')
+      }
+    }).catch((err) => {
+      console.log(err)
+      window.alert('메세지 전송에 실패하였습니다.')
+    })
+  }
+
+  const onChangeFormValue = (e: any) => {
+    const { name, value } = e.target
+    setFormValue({ ...formValue, [name]: value })
+  }
+
+  // const onClickSend = async () => {
+  // const result = await web.chat.postMessage({
+  //   text: 'hello world',
+  //   channel: id
+  // })
+  // console.log(`Successfully send message ${result.ts} in conversation ${conversationId}`);
+  // }
+
+>>>>>>> 3695e21 (Topic : 슬랙 token Env)
   return (
     <SectionLayout>
       <DashboardTitle section="CONTACT" heading="Need to get in touch?">
